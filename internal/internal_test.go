73dda8d1ba05e84ea234973f6f098bdcede3a2ba
George Thomas <georgetamayothomas@gmail.com>
2018-08-28 15:35:59 +0100

    Add failing test with websocket

--- internal/internal_test.go
+++ internal/internal_test.go
@@ -1,11 +1,14 @@
 package internal
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	libhoney "github.com/honeycombio/libhoney-go"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/gorilla/websocket"
 )
 
 func TestParseTraceHeader(t *testing.T) {
@@ -35,4 +38,49 @@ func TestURLHostHeader(t *testing.T) {
 	AddRequestProps(req, ev)
 	fs := ev.Fields()
 	assert.Equal(t, "example.com", fs["request.host"])
+}
+
+func TestHijackingWebsockets(t *testing.T) {
+	clientSent := "helloWorld"
+	srvReceived := ""
+	srvClose := make(chan struct{}, 1)
+
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Comment out this line to see the working behaviour without the internal.ResponseWriter.
+		w = NewResponseWriter(w)
+		defer func() {
+			srvClose <- struct{}{}
+		}()
+
+		upgrader := websocket.Upgrader{}
+		c, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			t.Error("upgrade:", err)
+			return
+		}
+		defer c.Close()
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			t.Log("read:", err)
+			return
+		}
+		srvReceived = string(message)
+	}))
+	defer srv.Close()
+
+	url := "ws" + srv.URL[len("http"):]
+	t.Logf("url: %s", url)
+
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer conn.Close()
+	conn.WriteMessage(websocket.TextMessage, []byte(clientSent))
+	<-srvClose
+
+	if clientSent != srvReceived {
+		t.Errorf("sent %s but received %s", clientSent, srvReceived)
+	}
 }
